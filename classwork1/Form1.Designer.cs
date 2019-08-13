ce9d636518e6d42524d8020eee2783264addef7d
Admin <Admin@DESKTOP-17CKR5L>
2019-08-13 12:27:07 +1200

    Add project files.

---
+++ classwork1/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace classwork1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGo = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.lbxOutput = new System.Windows.Forms.ListBox();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnGo
+            // 
+            this.btnGo.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnGo.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnGo.Location = new System.Drawing.Point(373, 121);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(135, 64);
+            this.btnGo.TabIndex = 0;
+            this.btnGo.Text = "Click Me";
+            this.btnGo.UseVisualStyleBackColor = false;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblOutput.Location = new System.Drawing.Point(396, 211);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(100, 23);
+            this.lblOutput.TabIndex = 1;
+            // 
+            // lbxOutput
+            // 
+            this.lbxOutput.FormattingEnabled = true;
+            this.lbxOutput.Location = new System.Drawing.Point(552, 90);
+            this.lbxOutput.Name = "lbxOutput";
+            this.lbxOutput.Size = new System.Drawing.Size(120, 95);
+            this.lbxOutput.TabIndex = 2;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(396, 249);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(138, 20);
+            this.txtInput.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lbxOutput);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.btnGo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.ListBox lbxOutput;
+        private System.Windows.Forms.TextBox txtInput;
+    }
+}
+
